7ddd793bac0b1b3fa89a26c1c0775c0b990f82aa
Isbel <isbel8ai@gmail.com>
2020-01-24 18:33:59 +0200

    Initialize project structure

---
+++ src/test/java/com/i8ai/training/storeapi/ProductStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i8ai.training.storeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
