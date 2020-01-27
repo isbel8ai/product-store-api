5b4ad118d9cb24b99a2a26696c23b7676610ac8a
Isbel <isbel8ai@gmail.com>
2020-01-27 15:32:46 +0200

    Add model Repositories

---
+++ src/main/java/com/i8ai/training/storeapi/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.i8ai.training.storeapi.repository;
+
+import com.i8ai.training.storeapi.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/i8ai/training/storeapi/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package com.i8ai.training.storeapi.repository;
+
+import com.i8ai.training.storeapi.domain.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+}
