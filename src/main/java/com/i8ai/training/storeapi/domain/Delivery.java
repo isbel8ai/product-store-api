405d2d1a4ab6f74015548050420e1315a9f4b6d6
Isbel <isbel8ai@gmail.com>
2020-01-28 18:10:30 +0200

    Add add entity Delivery

---
+++ src/main/java/com/i8ai/training/storeapi/domain/Delivery.java
@@ -0,0 +1,31 @@
+package com.i8ai.training.storeapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Column(nullable = false)
+    private Date delivered;
+    @NotNull
+    @Column(nullable = false)
+    private Double amount;
+    @ManyToOne
+    @JoinColumn(nullable = false, updatable = false)
+    private Lot from;
+    @ManyToOne
+    @JoinColumn(nullable = false, updatable = false)
+    private Shop to;
+}
