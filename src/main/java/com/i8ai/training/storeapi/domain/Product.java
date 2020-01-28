790e9782226299dbfd5a584ebd71d38a8b4bfc05
Isbel <isbel8ai@gmail.com>
2020-01-28 12:31:21 +0200

    Refactor entities Product and Shop to adding identifier generation

--- src/main/java/com/i8ai/training/storeapi/domain/Product.java
+++ src/main/java/com/i8ai/training/storeapi/domain/Product.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -15,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     @Column(nullable = false, unique = true)
