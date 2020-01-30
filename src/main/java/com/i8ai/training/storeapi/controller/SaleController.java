3a5a533e369fe905c33305b5dde748c37fed6638
Isbel <isbel8ai@gmail.com>
2020-01-30 17:17:41 +0200

    Add controller class to manage sales

---
+++ src/main/java/com/i8ai/training/storeapi/controller/SaleController.java
@@ -0,0 +1,38 @@
+package com.i8ai.training.storeapi.controller;
+
+import com.i8ai.training.storeapi.domain.Sale;
+import com.i8ai.training.storeapi.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sale")
+public class SaleController {
+    private final SaleService saleService;
+
+    @Autowired
+    public SaleController(SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    @GetMapping
+    public List<Sale> getSales(@RequestParam(required = false) Date start,
+                               @RequestParam(required = false) Date end,
+                               @RequestParam(required = false) Long productId,
+                               @RequestParam(required = false) Long shopId) {
+        return saleService.getSales(start, end, productId, shopId);
+    }
+
+    @PostMapping
+    public Sale registerSale(@RequestBody Sale newSale) {
+        return saleService.registerSale(newSale);
+    }
+
+    @DeleteMapping("/{saleId}")
+    public void deleteSale(@PathVariable Long saleId) {
+        saleService.deleteSale(saleId);
+    }
+}
